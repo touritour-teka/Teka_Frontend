187a0287222d39ce0a71ab5975288677aa037fa4
oxjadex <oharin765@gmail.com>
2025-04-15 08:59:32 +0900

    feat(packages): ui의 Text, Flex 추가

---
+++ packages/ui/src/Flex/Column.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { flex } from '@teka/utils';
+import styled from 'styled-components';
+import type { FlexProps } from './Flex.type';
+
+const Column = ({
+  children,
+  gap,
+  justifyContent,
+  alignItems,
+  width,
+  height,
+  style,
+}: FlexProps) => {
+  return (
+    <StyledColumn style={{ gap, justifyContent, alignItems, width, height, ...style }}>
+      {children}
+    </StyledColumn>
+  );
+};
+
+export default Column;
+
+const StyledColumn = styled.div`
+  ${flex({ flexDirection: 'column' })}
+`;
