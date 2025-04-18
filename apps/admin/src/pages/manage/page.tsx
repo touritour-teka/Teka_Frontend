587bc2dfcbe25e6ccb255e4238d6de3598dc5bc3
junghongseop <a01082372487@gmail.com>
2025-04-19 03:37:55 +0900

    feat(admin): 채팅방 관리 페이지 퍼블리싱

--- apps/admin/src/pages/manage/page.tsx
+++ apps/admin/src/pages/manage/page.tsx
@@ -1,5 +1,40 @@
+import RoomList from '@/components/manage/RoomList';
+import RoundedButton from '@/components/RoundedButton';
+import { flex } from '@teka/utils';
+import styled from 'styled-components';
+
 const ManagePage = () => {
-    return <>채팅방 관리</>
+  return (
+    <StyledManagePage>
+      <ManagePageBox>
+        <ButtonWrapper>
+          <RoundedButton width={89} onClick={() => {}}>
+            방 개설
+          </RoundedButton>
+        </ButtonWrapper>
+        <RoomList />
+      </ManagePageBox>
+    </StyledManagePage>
+  );
 };
 
 export default ManagePage;
+
+const StyledManagePage = styled.div`
+  ${flex({ justifyContent: 'center' })}
+  width: 100%;
+  height: 100vh;
+`;
+
+const ManagePageBox = styled.div`
+  ${flex({ flexDirection: 'column' })}
+  width: 100%;
+  max-width: 400px;
+  padding-top: 87px;
+  gap: 12px;
+`;
+
+const ButtonWrapper = styled.div`
+  ${flex({ justifyContent: 'flex-end' })}
+  padding-right: 16px;
+`;
