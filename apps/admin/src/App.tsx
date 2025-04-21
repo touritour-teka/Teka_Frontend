ab5b196569e6ac030b5b229331b48adbda973728
junghongseop <a01082372487@gmail.com>
2025-04-21 15:38:29 +0900

    fix(admin): 라우터 추가

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import LoginPage from './pages/login/page';
 import SignupPage from './pages/signup/page';
 import ManagePage from './pages/manage/page';
+import RoomCreatePage from './pages/room/create/page';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/manage" element={<ManagePage />} />
+        <Route path="/room/create" element={<RoomCreatePage />} />
       </Routes>
     </Router>
   );
