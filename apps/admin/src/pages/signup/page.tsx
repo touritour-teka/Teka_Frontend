6c489273500eca232cf4a5c78d5f36e22984881e
junghongseop <a01082372487@gmail.com>
2025-04-22 11:25:39 +0900

    feat(admin): 훅 적용 및 로직 수정

--- apps/admin/src/pages/signup/page.tsx
+++ apps/admin/src/pages/signup/page.tsx
@@ -1,25 +1,67 @@
 import Button from '@/components/Button';
-import { ButtonInput, Column, PreviewInput } from '@teka/ui';
+import { ButtonInput, Column, PreviewInput, Text } from '@teka/ui';
 import { flex } from '@teka/utils';
 import styled from 'styled-components';
+import { useInput, useSingupAction, useVerificationAction } from './singup.hooks';
+import { color } from '@teka/design-system';
 
 const SignupPage = () => {
+  const { signup, handleSignupChange } = useInput();
+  const { handleSignup } = useSingupAction(signup);
+  const { handleValidate, isSuccess, isError } = useVerificationAction(signup);
+
+  const isFormValid = signup.username && signup.password && signup.password_confirm;
+  const isPasswordMatching = signup.password === signup.password_confirm;
+
   return (
     <StyledSignupPage>
       <SignupPageBox>
         <img src="/logo.svg" alt="logo" />
         <Column gap={122} width="100%" alignItems="center">
           <Column gap={32} width="100%" alignItems="center">
-            <ButtonInput
-              buttonText="중복 확인"
-              onClick={() => {}}
-              label="아이디"
-              placeholder="아이디를 입력해주세요"
+            <Column gap={4} width="100%" alignItems="flex-start">
+              <ButtonInput
+                buttonText="중복 확인"
+                name="username"
+                onClick={handleValidate}
+                label="아이디"
+                width="100%"
+                placeholder="아이디를 입력해주세요"
+                onChange={handleSignupChange}
+                value={signup.username}
+                enabled={!!signup.username}
+              />
+              {isError && (
+                <Text fontType="regular12" color={color.red900}>
+                  중복된 아이디입니다
+                </Text>
+              )}
+              {isSuccess && (
+                <Text fontType="regular12" color={color.blue800}>
+                  사용 가능한 아이디입니다
+                </Text>
+              )}
+            </Column>
+            <PreviewInput
+              label="비밀번호"
+              placeholder="비밀번호를 입력해주세요"
+              width="100%"
+              name="password"
+              onChange={handleSignupChange}
             />
-            <PreviewInput label="비밀번호" placeholder="비밀번호를 입력해주세요" />
-            <PreviewInput label="비밀번호" placeholder="비밀번호를 입력해주세요" />
+            <PreviewInput
+              width="100%"
+              label="비밀번호 재입력"
+              placeholder="비밀번호를 다시 입력해주세요"
+              name="password_confirm"
+              onChange={handleSignupChange}
+              isError={signup.password !== signup.password_confirm}
+              errorMessage="비밀번호가 일치하지 않습니다."
+            />
           </Column>
-          <Button onClick={() => {}}>가입 완료</Button>
+          <Button onClick={handleSignup} variant={isFormValid && isPasswordMatching && isSuccess ? 'primary' : 'disabled'}>
+            가입 완료
+          </Button>
         </Column>
       </SignupPageBox>
     </StyledSignupPage>
