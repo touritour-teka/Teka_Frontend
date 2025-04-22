d010e32c9e94d002d3410ff31cf3e4a9c03c01ff
junghongseop <a01082372487@gmail.com>
2025-04-22 11:24:43 +0900

    chore(admin): enabled 추가

--- apps/admin/src/services/admin/queries.ts
+++ apps/admin/src/services/admin/queries.ts
@@ -3,10 +3,11 @@ import { useQuery } from '@tanstack/react-query';
 import { getValidate } from './api';
 import { GetValidateReq } from '@/types/admin/remote';
 
-export const useValidateQuery = (username: GetValidateReq) => {
+export const useValidateQuery = ({ username }: GetValidateReq) => {
   const { data, ...restQuery } = useQuery({
-    queryKey: [KEY.VALIDATE],
-    queryFn: () => getValidate(username),
+    queryKey: [KEY.VALIDATE, username],
+    queryFn: () => getValidate({ username }),
+    enabled: false,
   });
 
   return { data, ...restQuery };
