a5e5ac2ef54d9eaa6ecc270a9d536f1d4f700a19
junghongseop <a01082372487@gmail.com>
2025-04-24 09:14:09 +0900

    fix(admin): 체크박스 컴포넌트 수정

--- apps/admin/src/components/CheckBox.tsx
+++ apps/admin/src/components/CheckBox.tsx
@@ -1,39 +1,33 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
-import { color, font } from '@teka/design-system';
+import React, { InputHTMLAttributes } from 'react';
+import styled from 'styled-components';
+import { color } from '@teka/design-system';
 import { IconCheck } from '@teka/icon';
 
-type CheckBoxProps = {
-  checked: boolean;
-  onChange: (checked: boolean) => void;
-  label?: string;
-  disabled?: boolean;
-  id?: string;
-};
-
-const CheckBox = ({ checked, onChange, label, disabled = false, id }: CheckBoxProps) => (
-  <LabelWrapper htmlFor={id} disabled={disabled}>
+const CheckBox = ({
+  name,
+  value,
+  onChange,
+  checked = false,
+}: InputHTMLAttributes<HTMLInputElement>) => (
+  <LabelWrapper>
     <HiddenCheckbox
-      id={id}
+      name={name}
       type="checkbox"
       checked={checked}
-      disabled={disabled}
-      onChange={(e) => onChange(e.target.checked)}
+      onChange={onChange}
+      value={value}
     />
-    <StyledCheckbox $checked={checked} $disabled={disabled}>
+    <StyledCheckbox $checked={checked}>
       {checked && <IconCheck width={12} height={12} color={color.white2} />}
     </StyledCheckbox>
-    {label && <TextLabel>{label}</TextLabel>}
   </LabelWrapper>
 );
 
 export default CheckBox;
 
-const LabelWrapper = styled.label<{ disabled: boolean }>`
+const LabelWrapper = styled.label`
   display: inline-flex;
   align-items: center;
-  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
-  opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
 `;
 
 const HiddenCheckbox = styled.input`
@@ -49,7 +43,7 @@ const HiddenCheckbox = styled.input`
   width: 1px;
 `;
 
-const StyledCheckbox = styled.div<{ $checked: boolean; $disabled: boolean }>`
+const StyledCheckbox = styled.div<{ $checked: boolean }>`
   width: 16px;
   height: 16px;
   background: ${({ $checked }) => ($checked ? color.blue800 : 'none')};
@@ -59,15 +53,4 @@ const StyledCheckbox = styled.div<{ $checked: boolean; $disabled: boolean }>`
   align-items: center;
   justify-content: center;
   transition: all 150ms;
-  ${({ $disabled }) =>
-    $disabled &&
-    css`
-      cursor: not-allowed;
-    `}
-`;
-
-const TextLabel = styled.span`
-  ${font.regular14};
-  margin-left: 8px;
-  color: ${color.gray800};
 `;
