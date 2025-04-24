bf31426025bcab2cea725445183039292f75f950
junghongseop <a01082372487@gmail.com>
2025-04-24 09:15:45 +0900

    feat(admin): 각 리액트 쿼리 작성

---
+++ apps/admin/src/services/room/mutations.ts
@@ -0,0 +1,35 @@
+import { useMutation } from '@tanstack/react-query';
+import { patchChatRoomClose, patchChatRoomOpen } from './api';
+import useApiError from '@/hooks/useApiError';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '@/constants/constant';
+
+export const usePatchRoomOpenMutation = (chatRoomId: number) => {
+  const { handleError } = useApiError();
+  const navigate = useNavigate();
+
+  const { mutate: chatRoomOpenMutate, ...restMutation } = useMutation({
+    mutationFn: () => patchChatRoomOpen(chatRoomId),
+    onSuccess: () => {
+      navigate(ROUTES.MANAGE);
+    },
+    onError: handleError,
+  });
+
+  return { chatRoomOpenMutate, ...restMutation };
+};
+
+export const usePatchRoomCloseMutation = (chatRoomId: number) => {
+  const { handleError } = useApiError();
+  const navigate = useNavigate();
+
+  const { mutate: chatRoomCloseMutate, ...restMutation } = useMutation({
+    mutationFn: () => patchChatRoomClose(chatRoomId),
+    onSuccess: () => {
+      navigate(ROUTES.MANAGE);
+    },
+    onError: handleError,
+  });
+
+  return { chatRoomCloseMutate, ...restMutation };
+};
