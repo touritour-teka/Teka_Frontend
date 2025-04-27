38ea35639d4c60981cb1e57803cfc43e6f7cae88
junghongseop <a01082372487@gmail.com>
2025-04-27 12:59:31 +0900

    feat(admin): 상세 페이지 리스트 컴포넌트 추가

---
+++ apps/admin/src/components/room/detail/RoomDetailList.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import RoomDetailListItem from './RoomDetailListItem';
+import { color } from '@teka/design-system';
+import { RoomDetail } from '@/types/room/client';
+
+interface RoomDetailListProps {
+  room: RoomDetail;
+  checked: string[];
+  onChange: (id: string) => void;
+}
+
+const RoomDetailList = ({ room, checked, onChange }: RoomDetailListProps) => {
+  return (
+    <StyledRoomDetailList>
+      {room.userList.map((prop) => (
+        <RoomDetailListItem
+          id={prop.id}
+          checked={checked.includes(prop.id.toString())}
+          onChange={() => onChange(prop.id.toString())}
+          name={prop.username}
+          phoneNumber={prop.phoneNumber}
+        />
+      ))}
+    </StyledRoomDetailList>
+  );
+};
+
+export default RoomDetailList;
+
+const StyledRoomDetailList = styled.div`
+  width: 100%;
+  border-top: 1px solid ${color.gray100};
+`;
